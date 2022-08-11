85f0e4040d43a47b87d3301541576341f2ce24ff
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-11 16:27:13 -0300

    requisito-3

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const INPUT_EMAIL = document.getElementById('email');
+const INPUT_PASSWORD = document.getElementById('password');
+const BUTTON_LOGIN = document.getElementById('btn-login');
+
+function login() {
+  BUTTON_LOGIN.addEventListener('click', function () {
+    if (INPUT_EMAIL.value === 'tryber@teste.com' && INPUT_PASSWORD.value === '123456') {
+      window.alert('Olá, Tryber!');
+    } else {
+      window.alert('Email ou senha inválidos.');
+    }
+  });
+}
+login();
